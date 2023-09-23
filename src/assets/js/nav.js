768e5e38e3697de2aefa383f5dd23ce9f417b09a
Sean McEvoy <sfmcevoy@gmail.com>
2023-09-23 06:24:30 -0500

    Add FAQ component on home page

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -22,3 +22,11 @@ function ariaExpanded() {
     csUL.setAttribute('aria-expanded', 'false');
   }
 }
+
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+for (const item of faqItems) {
+  const onClick = () => {
+    item.classList.toggle('active');
+  };
+  item.addEventListener('click', onClick);
+}
